c5dd9661c9b29503728f2e0af93ae9c9799128ef
Anders Bach Madsen <anders@uber.com>
2015-08-11 14:31:31 +0200

    Simplify setup.py to not parse requirements files

--- setup.py
+++ setup.py
@@ -1,50 +1,17 @@
 #!/usr/bin/env python
-import collections
-import pip
-import sys
 
+import sys
 from setuptools import setup, find_packages
 
-
-def get_install_requirements(filename):
-    ReqOpts = collections.namedtuple('ReqOpts', ['skip_requirements_regex',
-                                                 'default_vcs', 'isolated_mode'])
-
-    kwargs = {
-        'options': ReqOpts(None, 'git', False)
-    }
-
-    if hasattr(pip.download, 'PipSession'):
-        kwargs['session'] = pip.download.PipSession()
-
-    requires = []
-    dependency_links = []
-
-    for ir in pip.req.parse_requirements(filename, **kwargs):
-        if ir is not None:
-            if hasattr(ir, 'link'):
-                if ir.link is not None:
-                    dependency_links.append(str(ir.link))
-            else:
-                if ir.url is not None:
-                    dependency_links.append(str(ir.url))
-
-            if ir.req is not None:
-                req = str(ir.req)
-                requires.append(req)
-
-    return requires, dependency_links
-
-
-install_requires, dependency_links = get_install_requirements(
-    'requirements.txt')
+install_requires = [
+    'tornado',
+    'psutil',
+    'mock',
+    'six',
+]
 
 if sys.version_info < (3, 0):
-    install_requires_2, dependency_links_2 = get_install_requirements('requirements-py2.txt')
-    install_requires += install_requires_2
-    dependency_links += dependency_links_2
-
-tests_require, _ = get_install_requirements('requirements-test.txt')
+    install_requires.append('ipcalc')
 
 
 def read_long_description(filename="README.md"):
@@ -63,10 +30,11 @@ setup(
     keywords=["monitoring", "tornado"],
     description="Library of standard monitoring hooks for the Tornado framework",
     install_requires=install_requires,
-    dependency_links=dependency_links,
     long_description=read_long_description(),
     test_suite="nose.collector",
-    tests_require=tests_require,
+    tests_require=[
+        'nose',
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
