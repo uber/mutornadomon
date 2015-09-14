5da4bdee0bfe8d0e6f272f3360586737ccf06564
Evgeni Kunev <evgeni@uber.com>
2015-09-14 11:23:06 +0300

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_long_description(filename="README.md"):
 
 setup(
     name="mutornadomon",
-    version="0.1.14",
+    version="0.2.0",
     author="James Brown",
     author_email="jbrown@uber.com",
     url="https://github.com/uber/mutornadomon",
