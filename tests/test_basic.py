380c2437ab207c00a5daae131b74a48e25fb70d4
Evgeni Kunev <evgeni@uber.com>
2015-10-02 11:32:38 +0300

    Fix tests to pass on 2.6

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -36,11 +36,9 @@ class TestBasic(tornado.testing.AsyncHTTPTestCase):
         resp = self.fetch('/mutornadomon')
         self.assertEqual(resp.code, 200)
         resp = json.loads(resp.body.decode('utf-8'))
-        self.assertDictContainsSubset(
-            {'requests': 2, 'localhost_requests': 2, 'private_requests': 2},
-            resp['counters']
-        )
-        self.assertIn('accumulated_callback_duration', resp['counters'])
+        expected = {'requests': 2, 'localhost_requests': 2, 'private_requests': 2}.items()
+        self.assertTrue(all(pair in resp['counters'].items() for pair in expected))
+        self.assertTrue('accumulated_callback_duration' in resp['counters'])
         self.assertEqual(resp['process']['cpu']['num_threads'], 5)
         assert resp['process']['cpu']['system_time'] < 1.0
 
