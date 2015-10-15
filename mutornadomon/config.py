8e98a51484f17bf95159581005ecb4501cbc7726
Emil Stanchev <stanchev@uber.com>
2015-10-14 17:11:19 -0700

    Revert "Bootstrap IOLoop utilization metric"
    This reverts commit c6c99d331982521b87dc1dbe31e77e8bed786d64.

--- mutornadomon/config.py
+++ mutornadomon/config.py
@@ -1,12 +1,9 @@
 from __future__ import absolute_import
 
-from tornado.ioloop import IOLoop
-
 from mutornadomon import MuTornadoMon
 from mutornadomon.external_interfaces.publish import PublishExternalInterface
 from mutornadomon.external_interfaces.http_endpoints import HTTPEndpointExternalInterface
 from mutornadomon.collectors.web import WebCollector
-from mutornadomon.collectors.ioloop_util import UtilizationCollector
 
 
 def initialize_mutornadomon(tornado_app=None, publisher=None, publish_interval=None,
@@ -26,18 +23,6 @@ def initialize_mutornadomon(tornado_app=None, publisher=None, publish_interval=N
     # Start collecting metrics and register endpoint with app
     monitor = MuTornadoMon(external_interface, **monitor_config)
     monitor.start()
-
-    ioloop = IOLoop.current()
-    run_callback = ioloop.run_callback
-    utilization_stat = UtilizationCollector(monitor)
-
-    def timed_run_calback(*args, **kwargs):
-        global stat
-
-        with utilization_stat:
-            return run_callback(*args, **kwargs)
-
-    ioloop.run_callback = timed_run_calback
 
     if tornado_app:
         web_collector = WebCollector(monitor, tornado_app)
