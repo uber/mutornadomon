09f7ca123424b50ae95004e8629d72a0e35617a6
Emil Stanchev <stanchev@uber.com>
2015-10-14 17:11:18 -0700

    Revert "Patch the add_callback method of the IOLoop"
    This reverts commit 0535d9261b10f2ff191975936657369ec1b1119b.

--- mutornadomon/config.py
+++ mutornadomon/config.py
@@ -28,20 +28,16 @@ def initialize_mutornadomon(tornado_app=None, publisher=None, publish_interval=N
     monitor.start()
 
     ioloop = IOLoop.current()
-    add_callback = ioloop.add_callback
+    run_callback = ioloop.run_callback
     utilization_stat = UtilizationCollector(monitor)
 
-    def measure_callback(callback):
-        def timed_callback(*args, **kwargs):
-            with utilization_stat:
-                return callback(*args, **kwargs)
+    def timed_run_calback(*args, **kwargs):
+        global stat
 
-        return timed_callback
+        with utilization_stat:
+            return run_callback(*args, **kwargs)
 
-    def add_timed_callback(callback, *args, **kwargs):
-        return add_callback(measure_callback(callback), *args, **kwargs)
-
-    ioloop.add_callback = add_timed_callback
+    ioloop.run_callback = timed_run_calback
 
     if tornado_app:
         web_collector = WebCollector(monitor, tornado_app)
