0ccc2d184e1b14f518958bc24c46d690ca711cb2
Markus Wüstenberg <markus@uber.com>
2016-02-24 10:32:54 +0100

    Fix version in __init__.py

--- mutornadomon/__init__.py
+++ mutornadomon/__init__.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import
 from .monitor import MuTornadoMon  # noqa
 from .config import initialize_mutornadomon  # noqa
 
-version_info = (0, 3, 3)
+version_info = (0, 4, 0)
 __name__ = 'MuTornadoMon'
-__author__ = 'jbrown@uber.com'
+__author__ = 'dev@uber.com'
 __version__ = '.'.join(map(str, version_info))
 __license__ = 'MIT'
 
