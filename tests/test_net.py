7bf872e5b7dc7ebe31b3b3a81501a815fa22be72
Goutham Nath <goutham@uber.com>
2016-04-17 20:29:34 -0700

    Remove byte string tests

--- tests/test_net.py
+++ tests/test_net.py
@@ -13,7 +13,6 @@ class TestIsLocalAddress(unittest.TestCase):
     def test_is_local_address_works_with_python2(self, is_python2_mock):
         """Test is_local_address method works for python2."""
         is_python2_mock.return_value = True
-        self.assertTrue(is_local_address(b'127.0.0.1'))
         self.assertTrue(is_local_address(u'127.0.0.1'))
         self.assertTrue(is_local_address('127.0.0.1'))
         self.assertFalse(is_local_address('126.0.0.10'))
@@ -22,7 +21,6 @@ class TestIsLocalAddress(unittest.TestCase):
     def test_is_local_address_works_with_python3(self, is_python2_mock):
         """Test is_local_address method works for python3."""
         is_python2_mock.return_value = False
-        self.assertTrue(is_local_address(b'127.0.0.1'))
         self.assertTrue(is_local_address(u'127.0.0.1'))
         self.assertTrue(is_local_address('127.0.0.1'))
         self.assertFalse(is_local_address('126.0.0.10'))
@@ -34,7 +32,6 @@ class TestIsPrivateAddress(unittest.TestCase):
     def test_is_private_address_works_with_python2(self, is_python2_mock):
         """Test is_private_address method works for python2."""
         is_python2_mock.return_value = True
-        self.assertTrue(is_private_address(b'127.0.0.1'))
         self.assertTrue(is_private_address(u'127.0.0.1'))
         self.assertTrue(is_private_address('127.0.0.1'))
         self.assertFalse(is_private_address('126.0.0.10'))
@@ -43,7 +40,6 @@ class TestIsPrivateAddress(unittest.TestCase):
     def test_is_private_address_works_with_python3(self, is_python2_mock):
         """Test is_private_address method works for python3."""
         is_python2_mock.return_value = False
-        self.assertTrue(is_private_address(b'127.0.0.1'))
         self.assertTrue(is_private_address(u'127.0.0.1'))
         self.assertTrue(is_private_address('127.0.0.1'))
         self.assertFalse(is_private_address('126.0.0.10'))
