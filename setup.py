56418d75856b200bd13eb33f75765d631455417d
James Westover <westover@uber.com>
2019-07-25 23:22:51 -0700

    Making support for 3.5 official

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Intended Audience :: System Administrators",
         "Operating System :: OS Independent",
