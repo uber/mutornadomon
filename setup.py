cba7b35a6fa9535751365cce6d9a1cf48b743f8c
James Westover <westover@uber.com>
2019-10-22 14:46:54 -0700

    Preparing release 0.5.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_long_description(filename="README.md"):
 
 setup(
     name="mutornadomon",
-    version="0.5.1.dev0",
+    version="0.5.1",
     author="Uber Technologies, Inc.",
     author_email="dev@uber.com",
     url="https://github.com/uber/mutornadomon",
